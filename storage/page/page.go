82d880d162b87667162ed69cba58648af0b47b75
Qitian Zeng <qzeng3@hawk.iit.edu>
2022-05-02 21:07:03 -0700

    page test

--- storage/page/page.go
+++ storage/page/page.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2021 Qitian Zeng
-// 
+//
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
--- storage/page/page_test.go
+++ storage/page/page_test.go
@@ -1,26 +1,28 @@
 // Copyright (c) 2021 Qitian Zeng
-// 
+//
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
 package page
 
 import (
-	"fmt"
+	"github.com/stretchr/testify/assert"
+	"goostub/common"
+	"math/rand"
 	"testing"
+	"unsafe"
 )
 
 func TestLSN(t *testing.T) {
 	page := NewPage()
+	numTries := 10
 
-	page.SetLSN(238521)
-	if page.GetLSN() != 238521 {
-		t.Error("LSN not correctly set\n")
-	}
-
-	data := page.GetData()
-
-	for i := 0; i < 8; i++ {
-		fmt.Println(data[i])
+	a := assert.New(t)
+	for i := 0; i < numTries; i++ {
+		lsn := common.LSN(rand.Int31())
+		page.SetLSN(lsn)
+		a.Equal(lsn, page.GetLSN())
+		data := page.GetData()
+		a.Equal(lsn, *(*common.LSN)(unsafe.Pointer(&data[offsetLSN])))
 	}
 }
